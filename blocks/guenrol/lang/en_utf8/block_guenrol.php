abd61517a5b25e4acabbb4f3b586f145b1d8c03f
Howard Miller <howardsmiller@googlemail.com>
2009-12-15 10:33:42 +0000

    deleted crappy version of guenrol

--- blocks/guenrol/lang/en_utf8/block_guenrol.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$string['blockname'] = 'GU Course Enrol';
-$string['dbnotconfigured'] = '<p>External database enrollment is not configured on this site</p>';
-$string['dbconnecterror'] = '<p>Error connecting to external enrolment database</p>';
-$string['numberexistingusers'] = '<p>Number of existing users = $a</p>';
-$string['numberusers'] = '<p>Number of users = $a</p>';
-?>
-
