67ff4fed557de442af810bca44eac9890a783004
Howard Miller <howard@e-learndesign.co.uk>
2009-12-19 16:16:23 +0000

    added copyright notice

--- blocks/wiforms/lang/en_utf8/block_wiforms.php
+++ blocks/wiforms/lang/en_utf8/block_wiforms.php
@@ -1,5 +1,30 @@
 <?php
 
+
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.com                                            //
+//                                                                       //
+// Copyright (C) 2009 onwards  E-Learn Design Limited                    //
+// http://www.e-learndesign.co.uk
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
 $string['configemail'] = 'Email address that all forms are sent to';
 $string['configsubject'] = 'Subject line for emails'; 
 $string['blockname'] = 'W.I. Forms';
--- blocks/wiforms/settings.php
+++ blocks/wiforms/settings.php
@@ -1,5 +1,29 @@
 <?php
 
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// NOTICE OF COPYRIGHT                                                   //
+//                                                                       //
+// Moodle - Modular Object-Oriented Dynamic Learning Environment         //
+//          http://moodle.com                                            //
+//                                                                       //
+// Copyright (C) 2009 onwards  E-Learn Design Limited                    //
+// http://www.e-learndesign.co.uk
+//                                                                       //
+// This program is free software; you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of the License, or     //
+// (at your option) any later version.                                   //
+//                                                                       //
+// This program is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+// GNU General Public License for more details:                          //
+//                                                                       //
+//          http://www.gnu.org/copyleft/gpl.html                         //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
 $settings->add(new admin_setting_configtext('block_wiforms_email', get_string('email', 'block_wiforms'),
                    get_string('configemail', 'block_wiforms'), '', PARAM_CLEAN));
 
