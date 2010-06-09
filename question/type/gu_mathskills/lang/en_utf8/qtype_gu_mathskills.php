01df4a4b40fdf34b02a54f8fc9d91bff91dd3549
Howard Miller <howardsmiller@googlemail.com>
2010-06-09 14:05:21 +0100

    adding Nialls maths skills question type

---
+++ question/type/gu_mathskills/lang/en_utf8/qtype_gu_mathskills.php
@@ -0,0 +1,8 @@
+<?php
+
+$string['addinggu_mathskills'] = 'Adding Maths Skills';
+$string['editinggu_mathskills'] = 'Editing Maths Skills';
+$string['gu_mathskills'] = 'Maths Skills (GU)';
+$string['noneofabove'] = 'None of the above.';
+
+
