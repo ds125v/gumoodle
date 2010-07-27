415890d2fe4c2043f1c67e7c745bc17ee88c5ec0
Howard Miller <howardsmiller@googlemail.com>
2010-07-27 14:24:21 +0100

    remotehtml block added

---
+++ blocks/remotehtml/lang/en_utf8/block_remotehtml.php
@@ -0,0 +1,15 @@
+<?PHP // $Id$ 
+      // block_html.php - created with Moodle 1.7 beta + (2006101003)
+
+
+$string['configcontent'] = 'Content';
+$string['configtitle'] = 'Block Title';
+$string['html'] = 'Remote HTML';
+$string['instanceidwrong'] = 'Incorrect instance ID. Check URL';
+$string['leaveblanktohide'] = 'leave blank to hide the title';
+$string['newhtmlblock'] = '(new Remote HTML block)';
+$string['notfrontpage'] = 'Not a front page block';
+$string['pasteurl'] = 'URL to paste at remote end';
+$string['remoteurl'] = 'Remote URL (overrides content)';
+
+?>
