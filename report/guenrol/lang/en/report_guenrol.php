df4a5b665ff48119f156bfdfa4a324a9e04afdfc
Howard Miller <howardsmiller@gmail.com>
2012-07-10 15:50:24 +0100

    basic enrolment report for course

---
+++ report/guenrol/lang/en/report_guenrol.php
@@ -0,0 +1,30 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings
+ *
+ * @package    report
+ * @subpackage guenrol
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['guenrol:view'] = 'View course participation report';
+$string['listofcodes'] = 'The following automatic enrolment codes have been set (in the Course Settings) for this course. Click on the code for the current student list:';
+$string['page-report-guenrol-x'] = 'Any participation report';
+$string['page-report-guenrol-index'] = 'Course participation report';
+$string['pluginname'] = 'UofG enrolment status';
+$string['title'] = 'Automatic enrolment status';
