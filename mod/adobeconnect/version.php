1dca0d2b8421150570a26867aef3407f371edb65
Howard Miller <howardsmiller@gmail.com>
2012-08-07 10:32:03 +0100

    adding adobe connect plugin

---
+++ mod/adobeconnect/version.php
@@ -0,0 +1,12 @@
+<?php // $Id: version.php,v 1.14.2.1 2011/07/21 22:46:30 adelamarre Exp $
+
+/**
+ * @package mod
+ * @subpackage adobeconnect
+ * @author Akinsaya Delamarre (adelamarre@remote-learner.net)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$module->version  = 2011072120;  // The current module version (Date: YYYYMMDDXX)
+$module->requires  = 2010120700; // Requires this Moodle version
+$module->cron     = 0;           // Period for cron to check this module (secs)
