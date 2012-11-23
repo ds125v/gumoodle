766180dabeefacf5bacb987f7b59c17ab0d156b7
David Scotson <david.scotson@glasgow.ac.uk>
2012-11-23 09:49:11 +0000

    document sqlite defaults

--- config-dist.php
+++ config-dist.php
@@ -38,28 +38,14 @@ $CFG = new stdClass();
 // will be stored.  This database must already have been created         //
 // and a username/password created to access it.                         //
 
-$CFG->dbtype    = 'pgsql';      // 'pgsql', 'mysqli', 'mssql', 'sqlsrv' or 'oci'
-$CFG->dblibrary = 'native';     // 'native' only at the moment
-$CFG->dbhost    = 'localhost';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'username';   // your database username
-$CFG->dbpass    = 'password';   // your database password
-$CFG->prefix    = 'mdl_';       // prefix to use for all table names
-$CFG->dboptions = array(
-    'dbpersist' => false,       // should persistent database connections be
-                                //  used? set to 'false' for the most stable
-                                //  setting, 'true' can improve performance
-                                //  sometimes
-    'dbsocket'  => false,       // should connection via UNIX socket be used?
-                                //  if you set it to 'true' or custom path
-                                //  here set dbhost to 'localhost',
-                                //  (please note mysql is always using socket
-                                //  if dbhost is 'localhost' - if you need
-                                //  local port connection use '127.0.0.1')
-    'dbport'    => '',          // the TCP port number to use when connecting
-                                //  to the server. keep empty string for the
-                                //  default port
-);
+$CFG->dbtype    = 'sqlite3';      // 'pgsql', 'mysqli', 'mssql', 'sqlsrv' or 'oci'
+$CFG->dblibrary = 'pdo';     // 'native' only at the moment
+$CFG->dbhost    = null;  // eg 'localhost' or 'db.isp.com' or IP
+$CFG->dbname    = null;     // database name, eg moodle
+$CFG->dbuser    = null;   // your database username
+$CFG->dbpass    = null;   // your database password
+$CFG->prefix    = '';       // prefix to use for all table names
+$CFG->dboptions = array();
 
 
 //=========================================================================
